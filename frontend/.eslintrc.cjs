215cc2edd5bcdb8f84d60d747f2e902f007b1443
ccconac <qwerpoiu91@naver.com>
2023-09-05 10:06:58 +0900

    chore: [Client] #1 Build development environment

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended"],
+  ignorePatterns: ["node_modules", "dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+  },
+};
---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.module.scss";
+
+const App = () => {
+  return (
+    <div>
+      <h2>Check-Pass</h2>
+    </div>
+  );
+};
+
+export default App;
