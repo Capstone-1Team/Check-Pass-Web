7e604a8e447a2c26cccb978766b36fb889835f38
ccconac <qwerpoiu91@naver.com>
2023-09-13 23:27:55 +0900

    feat: [Client] #4 Implement light/dark mode in Login Page

--- frontend/src/Pages/LoginPage.tsx
+++ frontend/src/Pages/LoginPage.tsx
@@ -1,20 +1,27 @@
 import styles from "../Styles/Pages/_loginPage.module.scss";
+import variable from "../Styles/_variable.module.scss";
 import LoginForm from "../Components/LoginPage/LoginForm";
+import sunIcon from "../Assets/Image/sun.png";
+import moonIcon from "../Assets/Image/moon.png";
+import { useState } from "react";
 
 const LoginPage = () => {
+  const [mode, setMode] = useState<boolean>(false);
+
+  const changeMode = () => {
+    setMode(!mode);
+  };
+
   return (
-    <div className={styles.background}>
+    <div className={mode ? `${variable.dark} ${styles.background}` : `${styles.background}`}>
+      <img src={mode ? moonIcon : sunIcon} onClick={changeMode} className={styles.mode} />
       <div className={styles.container}>
         <div className={styles.logo_container}>
           <div className={styles.title}>
             <span>CHECKPASS</span>
           </div>
           <div className={styles.text}>
-            <span>
-              CHECKPASS는 어쩌구저쩌구꿍시꿍시
-              <br />
-              이러한 플랫폼입니다 어쩌구저쩌구꿍시꿍시
-            </span>
+            <span>우리들의 편리한 출결을 위한 서비스 CHEKCPASS</span>
           </div>
         </div>
         <LoginForm />
