f54a5f8df12ccd17e5f6e87ad9ad44e41ab27066
ccconac <qwerpoiu91@naver.com>
2023-09-18 02:16:29 +0900

    feat: [Client] #5 Implement header component

---
+++ frontend/src/Components/MainPage/Header.tsx
@@ -0,0 +1,21 @@
+import styles from "../../Styles/Component/_header.module.scss";
+import { useTheme } from "../../Contexts/DarkModeContext";
+
+const Header = () => {
+  const { isDarkMode, toggleTheme } = useTheme();
+  return (
+    <div>
+      <header className={isDarkMode ? `${styles.header} ${styles.dark}` : `${styles.header}`}>
+        <nav className={styles.wrap_header}>
+          <h2>CHECKPASS</h2>
+          <div className={styles.area_util}>
+            <button className={styles.click_search}></button>
+            <button onClick={toggleTheme} className={styles.click_darkmode}></button>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
