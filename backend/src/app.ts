783c99a73e980ae83c5558b832e380c391c339df
ccconac <qwerpoiu91@naver.com>
2023-10-01 23:32:17 +0900

    chore: [Server] #12 Install cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,17 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+/* POST code 추가 */
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
