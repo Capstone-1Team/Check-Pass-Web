4a13b31de35f8cbb82f70fd1baef973ab340d010
ccconac <qwerpoiu91@naver.com>
2023-10-12 17:33:13 +0900

    feat: [Client] #17 Implement Attendance Page

--- frontend/src/Pages/AttendancePage.tsx
+++ frontend/src/Pages/AttendancePage.tsx
@@ -1,7 +1,16 @@
+import Header from "../Components/Header";
+import styles from "../Styles/Pages/AttendancePage/_attendancePage.module.scss";
+import { useTheme } from "../Contexts/DarkModeContext";
+import BeaconPage from "./BeaconPage";
+
 const AttendancePage = () => {
+  const { isDarkMode } = useTheme();
+
   return (
-    <div>
-      <h1>Attendance Page</h1>
+    <div className={isDarkMode ? `${styles.dark} ${styles.container}` : `${styles.container}`}>
+      <Header />
+      <BeaconPage />
+      <button className={styles.code_button}>코드 생성기</button>
     </div>
   );
 };
